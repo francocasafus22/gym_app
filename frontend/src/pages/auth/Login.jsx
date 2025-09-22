2f8cc974e9f5ed86f8d0675967952cf00076d7f0
Vasillyy <cirom5774@gmail.com>
2025-09-22 20:18:44 -0300

    Login

---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+        <>
+            <div className="flex justify-center items-center mx-auto min-h-screen">
+                <h1 className="text-6xl text-orange-400 font-bold">Login</h1>
+            </div>
+        </>
+    );
+}
