fcf7a99a1990090ca4524076fb8a74c18cbf144a
francocasafus22 <francocasafus55@gmail.com>
2025-09-22 19:51:11 -0300

    agregue el componente

--- frontend/src/pages/Perfil.jsx
+++ frontend/src/pages/Perfil.jsx
@@ -2,7 +2,7 @@ export default function Perfil() {
     return (
         <>
             <div className="flex justify-center items-center mx-auto min-h-screen">
-                <h1 className="text-6xl text-orange-400 font-bold">aca va tu Perfil :v</h1>
+                <h1 className="text-6xl text-orange-400 font-bold">aca va tu Perfil :vs</h1>
             </div>
         </>
     );
