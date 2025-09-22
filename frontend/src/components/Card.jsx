7eed4363bf2060b0540be133c89ab1d92fc049cd
Vasillyy <cirom5774@gmail.com>
2025-09-22 20:14:48 -0300

    Card Componente

---
+++ frontend/src/components/Card.jsx
@@ -0,0 +1,9 @@
+export default function Card() {
+    return (
+        <>
+            <div className="flex justify-center items-center mx-auto min-h-screen">
+                <h1 className="text-6xl text-orange-400 font-bold">Card</h1>
+            </div>
+        </>
+    );
+}
