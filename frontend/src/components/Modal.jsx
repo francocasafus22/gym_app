efe2db20750a695ff0fd87aea91868ac74d3da95
francocasafus22 <francocasafus55@gmail.com>
2025-10-08 23:13:23 -0300

    agregue el Modal

---
+++ frontend/src/components/Modal.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Modal({ isOpen, onClose, children }) {
+  if (!isOpen) return null; // No renderiza nada si no está abierto
+
+  return (
+    <div className="fixed inset-0 bg-black/20 backdrop-blur-xs flex justify-center items-center z-50">
+      <div className="bg-primary border-1 border-border rounded-lg w-full max-w-xl p-20 pb-10 mx-5 relative flex flex-col justify-between">
+        <button
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          onClick={onClose}
+        >
+          X
+        </button>
+
+        {children}
+
+        <div className="flex justify-center">
+          <button
+            className="bg-accent text-accent-foreground px-4 py-2 rounded w-50 mt-10"
+            onClick={() => onClose()}
+          >
+            Agregar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
