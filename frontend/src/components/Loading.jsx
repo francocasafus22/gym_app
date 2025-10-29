51b209caf5d71b0c125904267da1c34da975f260
francocasafus22 <francocasafus55@gmail.com>
2025-10-29 13:31:37 -0300

    style loading: cambiarle el color

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,7 +1,9 @@
-export default function Loading() {
+export default function Loading({ color = "accent" }) {
   return (
     <div className="flex justify-center items-center ">
-      <div className="w-8 h-8 border-4 border-accent border-t-transparent rounded-full animate-spin"></div>
+      <div
+        className={`w-8 h-8 border-4 border-${color} border-t-transparent rounded-full animate-spin`}
+      ></div>
     </div>
   );
 }
