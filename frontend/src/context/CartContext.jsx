78b2ce3772402a44256b70d6770d02a5ad90e895
Vasillyy <cirom5774@gmail.com>
2025-12-03 00:25:58 -0300

    fix cartContext

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -4,35 +4,36 @@ export const CartContext = createContext();
 
 export function CartProvider({ children }) {
 
-    const [cart, setCart] = useState(JSON.parse(localStorage.getItem("CART_ITEMS") || []));
+  const [cart, setCart] = useState(JSON.parse(localStorage.getItem("CART_ITEMS") || "[]"));
 
-    const addProduct = (producto) => {
-        const existing = JSON.parse(localStorage.getItem("CART_ITEMS")) || [];
 
-        // Buscar si el producto ya existe
-        const index = existing.findIndex(p => p.productId === producto._id);
+  const addProduct = (producto) => {
+    const existing = JSON.parse(localStorage.getItem("CART_ITEMS")) || [];
 
-        if (index === -1) {
-            // No existe, lo agrego con cantidad 1
-            existing.push({ productId: producto._id, nombre: producto.nombre, precio: producto.precio, quantity: 1 });
-        } else {
-            // Ya existe, aumento la cantidad
-            existing[index].quantity += 1;
-        }
+    // Buscar si el producto ya existe
+    const index = existing.findIndex(p => p.productId === producto._id);
 
-        // Guardar el array actualizado
-        localStorage.setItem("CART_ITEMS", JSON.stringify(existing));
-        setCart(existing)
-    };
-  
-    const deleteProduct = (producto) => {
-      const existing = JSON.parse(localStorage.getItem("CART_ITEMS")) || [];
+    if (index === -1) {
+      // No existe, lo agrego con cantidad 1
+      existing.push({ productId: producto._id, nombre: producto.nombre, precio: producto.precio, quantity: 1 });
+    } else {
+      // Ya existe, aumento la cantidad
+      existing[index].quantity += 1;
+    }
 
-      const updated = existing.filter(p => p.productId !== producto.productId);
+    // Guardar el array actualizado
+    localStorage.setItem("CART_ITEMS", JSON.stringify(existing));
+    setCart(existing)
+  };
 
-      localStorage.setItem("CART_ITEMS", JSON.stringify(updated));
-      setCart(updated);
-    };
+  const deleteProduct = (producto) => {
+    const existing = JSON.parse(localStorage.getItem("CART_ITEMS")) || [];
+
+    const updated = existing.filter(p => p.productId !== producto.productId);
+
+    localStorage.setItem("CART_ITEMS", JSON.stringify(updated));
+    setCart(updated);
+  };
 
   return (
     <CartContext.Provider
